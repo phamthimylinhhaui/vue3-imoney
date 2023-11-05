4a980f0f15f5bbdde6d3f6fc5d152f972cc81c71
hapo-linhptm <linh.ptm@haposoft.com>
2023-11-06 06:37:05 +0700

    install project

---
+++ src/layouts/auth.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1>this is auth layout</h1>
+    <slot></slot>
+</template>
--- src/main.js
+++ src/main.js
@@ -2,4 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+import './assets/style/tailwind.css';
+import './assets/style/global.css';
+
+import { registerGlobalComponent } from './utils/import';
+
+const app = createApp(App);
+
+registerGlobalComponent(app)
+
+app.use(router);
+app.mount('#app')
